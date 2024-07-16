9e9761900fb3ffa506a5f5400cb5353d41de2282
dipu-37 <razibul1105nstu@gmail.com>
2024-07-17 00:20:47 +0700

    CRUT

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000
 
@@ -30,16 +30,64 @@ async function run() {
     await client.connect();
     const coffeeCollection = client.db('coffeeDB').collection('coffee');
 
-    app.post('/coffee',async(req,res)=>{
-        const newCoffee = req.body;
-        console.log(newCoffee)
-        const result = await coffeeCollection.insertOne(newCoffee);
-        res.send(result);
+
+    // store data in mongodb 
+    app.post('/coffee', async (req, res) => {
+      const newCoffee = req.body;
+      console.log(newCoffee);
+      const result = await coffeeCollection.insertOne(newCoffee);
+      res.send(result);
     })
-    
-   
+
+    // view all data 
+    app.get('/coffee', async (req, res) => {
+      const cursor = coffeeCollection.find();
+      const results = await cursor.toArray();
+      res.send(results);
+
+    })
+
+    // delete data 
+    app.delete('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await coffeeCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    // for update 
+
+    app.get('/coffee/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await coffeeCollection.findOne(query);
+      res.send(result);
+    })
+
+    app.put('/coffee/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const options = { upsert: true };
+      const updatedCoffee = req.body;
+
+      const coffee = {
+          $set: {
+              name: updatedCoffee.name, 
+              quantity: updatedCoffee.quantity, 
+              supplier: updatedCoffee.supplier, 
+              taste: updatedCoffee.taste, 
+              category: updatedCoffee.category, 
+              details: updatedCoffee.details, 
+              photo: updatedCoffee.photo
+          }
+      }
+
+      const result = await coffeeCollection.updateOne(filter, coffee, options);
+      res.send(result);
+  })
+
   } finally {
-    
+
     // await client.close();
   }
 }
